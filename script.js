38a397160fb92925df7c428525629992554acb14
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-07 23:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+ document.addEventlistner("DOMContentLoded", () => {
+	 document.getelementById("btn").addEventListner(("Click", (e) => {
+		 e.preventDefault();
+const firstName = document.getElementById("firstname").value;
+const secondName =document.getElementById("lastname").value;
+const email = document.getElementById("email").value;
+const phoneNumber = document.getElementById("phonenumber").value;
+https://www.svgrepo.com/show/345221/three-dots.svg
+		const message = (`FirstName:${firstName}\n LastName:${last} \n Email:${email}\n PhoneNumber:${number}`); 
+	aleart(message);
+	 });					  
